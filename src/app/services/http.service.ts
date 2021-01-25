0a337521c79fb1876ebf877a6e564de81922da64
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-01-25 22:00:59 +0900

    みため

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,54 @@
+export const getPosts = () => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    const method = "GET";
+    const url = "http://localhost:3004/posts";
+
+    xhr.open(method, url, true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        var status = xhr.status;
+        if (status === 0 || (status >= 200 && status < 400)) {
+          console.log(JSON.parse(xhr.responseText));
+          resolve(JSON.parse(xhr.responseText));
+          // console.log(xhr.responseText);
+          // resolve(xhr.responseText);
+        } else {
+          console.error(JSON.parse(xhr.responseText));
+          reject(JSON.parse(xhr.responseText));
+        }
+      }
+    };
+    xhr.send();
+  });
+};
+
+export const addTestData = () => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    const method = "POST";
+    const url = "http://localhost:3004/posts";
+
+    xhr.open(method, url, true);
+
+    // application/jsonは？
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        var status = xhr.status;
+        if (status === 0 || (status >= 200 && status < 400)) {
+          // console.log(xhr.responseText);
+          resolve(xhr.responseText);
+        } else {
+          console.error(JSON.parse(xhr.responseText));
+          reject(JSON.parse(xhr.responseText));
+        }
+      }
+    };
+    xhr.send(
+      "data=じゅげむ じゅげむ ごこうのすりきれ かいじゃりすいぎょの すいぎょうまつ うんらいまつ ふうらいまつ くうねるところに すむところ やぶらこうじの ぶらこうじ パイポパイポ パイポのシューリンガン シューリンガンのグーリンダイ グーリンダイのポンポコピーのポンポコナーの ちょうきゅうめいのちょうすけ"
+    );
+    xhr.abort();
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-let hello: string = "hello";
+import { getPosts } from "./app/services/http.service";
+import "./style.css";
 
-alert(hello);
+getPosts().then((res: any) => {
+  res.forEach((element: { data: any }) => {
+    return contentsEl?.insertAdjacentHTML(
+      "beforeend",
+      `<div class="item">${element.data}</div>`
+    );
+  });
+});
+
+const contentsEl = document.getElementById("contents");
